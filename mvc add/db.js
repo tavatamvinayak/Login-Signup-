5a3197a8435bc89c20c9c0006714c308006ffcdc
vinayak tavatam <107667781+tavatamvinayak@users.noreply.github.com>
2023-05-08 11:24:49 +0530

    db changes

--- mvc add/db.js
+++ mvc add/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const dbConnect = async()=>{
-    mongoose.connect('mongodb+srv://tavatamvinayak:Minakshi@cluster0.taouj4t.mongodb.net/form?retryWrites=true&w=majority').then(() => {
+    mongoose.connect('mongodb+srv://tavatamvinayak:<Password>@cluster0.taouj4t.mongodb.net/form?retryWrites=true&w=majority').then(() => {
         console.log('connect database successfuly')
     }).catch((err) => {
         console.error(err)
